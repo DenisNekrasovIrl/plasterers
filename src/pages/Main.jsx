efec272429d1f5db15d79e98b2eb0f990555894b
Некрасов Денис <denis.nekrasov@irlix.ru>
2023-03-16 10:41:00 +0400

    dwqDDWW!S

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -21,53 +21,21 @@ import Reviews from "../components/reviews/Reviews";
 import Stages from "../components/stages/Stages";
 
 export default function Main() {
-  const getBanner = async function () {
-    const res = await fetch(
-      "/api/v1?object=account&action=list&actionObject=activeBanners&date=2023-03-10",
-      {
-        method: "GET",
-        headers: {
-          "X-Yandex-API-Key": "150f153e-c29e-4cf2-a169-29375400f9b1"
-        },
-      }
-    );
-    console.log(res)
-    const text = await res.text();
-    const data = new window.DOMParser().parseFromString(text, "text/xml");
-    console.log(data.getElementsByTagName("data")[0]);
-    const resPlace = await fetch(
-      `/api/v1?object=account&action=list&actionObject=place`,
-      {
-        method: "GET",
-        headers: {
-          "X-Yandex-API-Key": "150f153e-c29e-4cf2-a169-29375400f9b1",
-        },
-      }
-    );
-    const textPlace = await resPlace.text();
-    const dataPlace = new window.DOMParser().parseFromString(
-      textPlace,
-      "text/xml"
-    );
-    console.log(dataPlace.getElementsByTagName("data")[0]);
-    const resBanner = await fetch(
-      `/api/v1?object=placement&action=info&mode=place&placeID=1327597`,
-      {
-        method: "GET",
-        headers: {
-          "X-Yandex-API-Key": "150f153e-c29e-4cf2-a169-29375400f9b1",
-        },
-      }
-    );
-    const textBanner = await resBanner.text();
-    const dataBanner = new window.DOMParser().parseFromString(
-      textBanner,
-      "text/xml"
-    );
-    console.log(dataBanner);
-  };
   useEffect(() => {
-    getBanner();
+    document.querySelector('.banner-a2').insertAdjacentHTML('afterbegin', `
+    <script>
+    window.yaContextCb.push(()=>{
+        Ya.adfoxCode.create({
+            ownerId: 1457688,
+            containerId: 'adfox_167894822106839019',
+            params: {
+                p1: 'cxpis',
+                p2: 'idjv'
+            }
+        })
+    })
+</script>
+    `)
   }, []);
   return (
     <>
@@ -82,7 +50,7 @@ export default function Main() {
           <HowWork />
           <Price />
           <Includes />
-
+          <div className='banner-a2' id="adfox_167894822106839019"></div>
           <Brigadier />
           <Guarantees />
           <Connection />
